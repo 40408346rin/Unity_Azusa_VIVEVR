cfb68aa76ae0f6b7668ace7d47440ca2aa444b20
Azusa <61855900+40408346rin@users.noreply.github.com>
2020-05-08 10:35:52 +0800

    完成VR發布PC

--- VIVEVR/Assets/Scripts/GameManager.cs
+++ VIVEVR/Assets/Scripts/GameManager.cs
@@ -1,6 +1,6 @@
-﻿using System.Runtime.InteropServices;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;   //引用 場景管理器 API
 using Valve.VR.InteractionSystem;   //引用 VR 互動 API
 public class GameManager : MonoBehaviour
 {
@@ -8,9 +8,23 @@ public class GameManager : MonoBehaviour
     public Text textBallCount;
     [Header("分數")]
     public Text textScore;
+    [Header("兩分球音效")]
+    public AudioClip soundTwo;
+    [Header("三分球音效")]
+    public AudioClip soundThree;
 
+    private AudioSource aud;
     private int ballCount = 5;
     private int score;
+
+    private ThreePoint threePoint;
+
+    private void Start()
+    {
+        aud = GetComponent<AudioSource>();
+
+        threePoint = FindObjectOfType<ThreePoint>();
+    }
 
 
     public void UseBall(GameObject ball)
@@ -29,9 +43,32 @@ public class GameManager : MonoBehaviour
 
         private void OnTriggerEnter(Collider other)
     {
+        if (threePoint.inThreePoint)
+        {
+            score += 3;
+            aud.PlayOneShot(soundThree);
+        }
+        else
+        {
+            score += 2;
+            aud.PlayOneShot(soundTwo);
+        }
             //分數遞減2
             score += 2;
             //更新文字
             textScore.text = "分數:" + score;
     }
+
+    public void Replay()
+    {
+        //場景管理器.載入場景("場景名稱")
+        SceneManager.LoadScene("投籃");
+    }
+
+    public void Quit()
+    {
+        //應用程式.離開遊戲() - 適用於 PC.手機
+        Application.Quit();
+    }
+
 }
